96e172cd3778d827e3035c003e69316844f40edd
renanbomtempo <renanbomtempo@gmail.com>
2026-09-01 17:22:19 -0300

    feat: Initialize a GLFW and create a window

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,19 @@
 // Baseline implementation following the Khronos Vulkan "Drawing a Triangle" tutorial.
 // Serves as the working foundation before being iteratively refactored into a custom
 // renderer API for the Nodens framework.
+#include <cstdint>
 #include <cstdlib>
+#include <exception>
+#include <print>
+#include <stdexcept>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 import vulkan;
 import std;
+
+constexpr uint32_t WINDOW_WIDTH = 800;
+constexpr uint32_t WINDOW_HEIGHT = 600;
 
 class HelloTriangleApplication
 {
@@ -23,15 +30,38 @@ public:
 private:
     void initVulkan()
     {
+        glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_WAYLAND);
+
+        if (!glfwInit())
+        {
+            throw std::runtime_error("GLFW Initialization failed.");
+        }
+
+        // GLFW was originally designed to work with OpenGL contexts,
+        // so we need to tell it not to create one.
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        // For now, we don't deal with resizable windows.
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Vulkan", nullptr, nullptr);
     }
 
     void mainLoop()
     {
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
     }
 
     void cleanup()
     {
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
+
+private:
+    GLFWwindow* window = nullptr;
 };
 
 int main()
@@ -43,7 +73,7 @@ int main()
     }
     catch (const std::exception& e)
     {
-        std::cerr << e.what() << std::endl;
+        std::println(stderr, "Error: {}", e.what());
         return EXIT_FAILURE;
     }
 
