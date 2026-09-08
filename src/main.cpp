ea1281f82962fdfd64b77ed848b1e16f82f042ca
renanbomtempo <renanbomtempo@gmail.com>
2026-09-08 18:30:44 -0300

    feat: Setup Extensions, Layers and the Instance

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,17 @@
 // Baseline implementation following the Khronos Vulkan "Drawing a Triangle" tutorial.
 // Serves as the working foundation before being iteratively refactored into a custom
 // renderer API for the Nodens framework.
+#include <algorithm>
 #include <cstdint>
 #include <cstdlib>
+#include <cstring>
 #include <exception>
 #include <print>
+#include <ranges>
 #include <stdexcept>
+#include <vector>
+#include <vk_video/vulkan_video_codec_av1std.h>
+#include <vulkan/vulkan_core.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
@@ -16,6 +22,14 @@ import std;
 
 constexpr uint32_t WINDOW_WIDTH = 800;
 constexpr uint32_t WINDOW_HEIGHT = 600;
+
+const std::vector<char const*> validationLayers{"VK_LAYER_KHRONOS_validation"};
+
+#ifdef NDEBUG
+constexpr bool enableValidationLayers = false;
+#else
+constexpr bool enableValidationLayers = true;
+#endif
 
 class HelloTriangleApplication
 {
@@ -28,6 +42,90 @@ public:
     }
 
 private:
+    std::vector<const char*> getRequiredInstanceExtensionsNames()
+    {
+        // “Vulkan is a platform-agnostic API, which means that you need an extension to interface
+        // with the window system. GLFW has a handy built-in function that returns the extension(s)
+        // it needs to do that which we can pass to the struct”
+        // - (“Khronos Vulkan Tutorial / Drawing a Triangle / Setup - Instance”)
+        uint32_t glfwExtensionCount = 0;
+        auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        std::vector extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+        return extensions;
+    }
+
+    void createInstance()
+    {
+        // DEFINE APPLICATION INFO /////////////////////////////////////////////////////////////////
+        constexpr vk::ApplicationInfo appInfo{.pApplicationName = "Hello Triangle",
+                                              .applicationVersion = VK_MAKE_VERSION(0, 0, 0),
+                                              .pEngineName = "Nyar",
+                                              .engineVersion = VK_MAKE_VERSION(0, 0, 0),
+                                              .apiVersion = vk::ApiVersion14};
+
+        // EXTENSIONS //////////////////////////////////////////////////////////////////////////////
+        // List available extensions
+        auto availableExtensionsNames =
+            context.enumerateInstanceExtensionProperties() |
+            std::views::transform([](const auto& extentionProperty)
+                                  { return std::string_view{extentionProperty.extensionName}; });
+        std::println("Available extensions:");
+        for (auto name : availableExtensionsNames)
+        {
+            std::println("\t{}", name);
+        }
+
+        // Check for required extensions
+        auto requiredExtensionsNames = getRequiredInstanceExtensionsNames();
+        auto unsupportedExtensionIt = std::ranges::find_if(
+            requiredExtensionsNames,
+            [&availableExtensionsNames](std::string_view requiredName)
+            { return !std::ranges::contains(availableExtensionsNames, requiredName); });
+        if (unsupportedExtensionIt != requiredExtensionsNames.end())
+        {
+            throw std::runtime_error("Required GLFW extension not supported: " +
+                                     std::string(*unsupportedExtensionIt));
+        }
+
+        // LAYERS //////////////////////////////////////////////////////////////////////////////////
+        // List available layers
+        auto layersNames =
+            context.enumerateInstanceLayerProperties() |
+            std::views::transform([](const auto& layerProperty)
+                                  { return std::string_view{layerProperty.layerName}; });
+        std::println("Available layers:");
+        for (auto name : layersNames)
+        {
+            std::println("\t{}", name);
+        }
+
+        // Check for required layers
+        std::vector<char const*> requiredLayers;
+        if (enableValidationLayers)
+        {
+            requiredLayers.assign(validationLayers.begin(), validationLayers.end());
+        }
+        auto unsupportedLayerIt = std::ranges::find_if(
+            requiredLayers,
+            [&](std::string_view req) { return !std::ranges::contains(layersNames, req); });
+
+        if (unsupportedLayerIt != requiredLayers.end())
+        {
+            throw std::runtime_error("Required layer not supported {}" +
+                                     std::string{*unsupportedLayerIt});
+        }
+
+        // CREATE INSTANCE /////////////////////////////////////////////////////////////////////////
+        vk::InstanceCreateInfo createInfo{
+            .pApplicationInfo = &appInfo,
+            .enabledLayerCount = static_cast<uint32_t>(requiredLayers.size()),
+            .ppEnabledLayerNames = requiredLayers.data(),
+            .enabledExtensionCount = static_cast<uint32_t>(requiredExtensionsNames.size()),
+            .ppEnabledExtensionNames = requiredExtensionsNames.data()};
+
+        instance = vk::raii::Instance(context, createInfo);
+    }
+
     void initVulkan()
     {
         glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_WAYLAND);
@@ -44,6 +142,8 @@ private:
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
         window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Vulkan", nullptr, nullptr);
+
+        createInstance();
     }
 
     void mainLoop()
@@ -62,6 +162,8 @@ private:
 
 private:
     GLFWwindow* window = nullptr;
+    vk::raii::Context context;
+    vk::raii::Instance instance = nullptr;
 };
 
 int main()
@@ -71,9 +173,14 @@ int main()
         HelloTriangleApplication app;
         app.run();
     }
-    catch (const std::exception& e)
+    catch (const vk::SystemError& err)
     {
-        std::println(stderr, "Error: {}", e.what());
+        std::println(stderr, "Vulkan Error {}", err.what());
+        return EXIT_FAILURE;
+    }
+    catch (const std::exception& err)
+    {
+        std::println(stderr, "Error: {}", err.what());
         return EXIT_FAILURE;
     }
 
